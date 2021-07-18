398e568ab42f4a437aebf7205f8ededacf97dd37
DennySPb <dennyspb@gmail.com>
2021-07-19 04:21:25 +0530

    Move FOD animation resources to standalone package [2/2] Change-Id: I4d1e483da50623c5d47dc5d831fa128b8b8e7ca4 Signed-off-by: DennySPb <dennyspb@gmail.com>

--- common.mk
+++ common.mk
@@ -1,11 +1,16 @@
 # Include package overlays
-PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/common
 DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/common
 
-#ifeq ($(EXTRA_FOD_ANIMATIONS),true)
-#DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/fod
-#PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/fod
-#endif
+ifeq ($(FOD_ANIMATIONS),true)
+PRODUCT_PACKAGES += \
+    FodAnimationResources
+endif
+
+ifeq ($(HAS_FOD),true)
+DEVICE_PACKAGE_OVERLAYS += vendor/themes/overlay/fod-icons
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += vendor/themes/overlay/fod-icons
+endif
 
 # Overlays
 include vendor/themes/overlays.mk
